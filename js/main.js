48d22053fa2f4699306c20685c15a17e58a4f2e7
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-02-13 18:17:59 -0300

    configuracao netlify

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Change "{}" to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('audio', {});
+
+// Expose player so it can be used from the console
+window.player = player;
