7d87a0ab89b96789e00a390ffda14985fa04bd14
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-06-04 12:39:46 -0300

    share buttons

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 //Indice do Player
 function seekToTime(time) {
-  var audio = document.getElementById('audioplayer');
+  var audio = document.getElementById("audioplayer");
   audio.currentTime = time;
   audio.play();
 }
@@ -18,3 +18,8 @@ function gtag() {
 gtag("js", new Date());
 gtag("config", "G-JSVR5N10GY");
 
+// Share button
+var a2a_config = a2a_config || {};
+a2a_config.onclick = 1;
+a2a_config.locale = "pt-BR";
+a2a_config.thanks = { postShare: true };
