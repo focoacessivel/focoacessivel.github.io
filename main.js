7844e02c78540c0b1f115bed1367ad8b135f5920
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-07-11 16:34:11 -0300

    mouseflow

--- main.js
+++ main.js
@@ -23,3 +23,14 @@ var a2a_config = a2a_config || {};
 a2a_config.onclick = 1;
 a2a_config.locale = "pt-BR";
 a2a_config.thanks = { postShare: true };
+
+
+
+//Mouseflow
+window._mfq = window._mfq || [];
+(function () {
+  var mf = document.createElement("script");
+  mf.type = "text/javascript"; mf.defer = true;
+  mf.src = "//cdn.mouseflow.com/projects/ddd06565-25a4-4b71-8557-2450f389e00c.js";
+  document.getElementsByTagName("head")[0].appendChild(mf);
+})();
