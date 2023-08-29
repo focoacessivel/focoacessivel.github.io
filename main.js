7cab26647da0830ce4c1a2414e71d1740b774b15
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-08-29 09:24:07 -0300

    GOOGLE TAG MANAGER

--- main.js
+++ main.js
@@ -10,27 +10,9 @@ function seekToTime(time) {
 
 window.player = player;
 
-// Analitycs
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-JSVR5N10GY");
 
 // Share button
 var a2a_config = a2a_config || {};
 a2a_config.onclick = 1;
 a2a_config.locale = "pt-BR";
 a2a_config.thanks = { postShare: true };
-
-
-
-//Mouseflow
-window._mfq = window._mfq || [];
-(function () {
-  var mf = document.createElement("script");
-  mf.type = "text/javascript"; mf.defer = true;
-  mf.src = "//cdn.mouseflow.com/projects/ddd06565-25a4-4b71-8557-2450f389e00c.js";
-  document.getElementsByTagName("head")[0].appendChild(mf);
-})();
