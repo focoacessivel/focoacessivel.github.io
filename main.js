4b21a6723d49d68ee74d2a8e1c489810954632ea
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-12-26 22:21:56 -0300

    atualização da estrutura da página

--- main.js
+++ main.js
@@ -16,3 +16,39 @@ var a2a_config = a2a_config || {};
 a2a_config.onclick = 1;
 a2a_config.locale = "pt-BR";
 a2a_config.thanks = { postShare: true };
+
+// Popover
+
+const list = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+list.map((el) => {
+  let opts = {
+    animation: false,
+  }
+  if (el.hasAttribute('data-content-id')) {
+    opts.content = document.getElementById(el.getAttribute('data-content-id')).innerHTML;
+    opts.html = true;
+  }
+  new bootstrap.Popover(el, opts);
+})
+
+
+
+function updateTextContent(idPattern, text, count) {
+  for (var i = 1; i <= count; i++) {
+      var element = document.getElementById(idPattern + i);
+      if (element) {
+          element.textContent = text;
+      }
+  }
+}
+
+function updateTextContentByClass(className, text) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++) {
+      elements[i].textContent = text;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  updateTextContentByClass('episodiosItem', 'Episódio');
+});
